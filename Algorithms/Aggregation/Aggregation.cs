089f16e424dba7cc8283196e78ef0c59ae2814de
d0rf47 <m.jay.falconi@gmail.com>
2021-11-11 18:12:04 -0500

    Aggregation and rotations

---
+++ Algorithms/Aggregation/Aggregation.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+
+namespace Algorithms
+{
+    /**
+    *   Is a means of combining data
+    *   from multiple datasets
+    *   often involves some sort of filter / manipulation
+    *   of the data in the process
+    */
+    class Aggregation
+    {
+        public static int[] MergeEvenValues(int[] arr1, int[] arr2)
+        {
+            ArrayList result = new ArrayList();
+            AddEvenFromArray (arr1, result);
+            AddEvenFromArray (arr2, result);
+
+            return (int[]) result.ToArray(typeof (int));
+        }
+
+        //Helper functions
+        public static void AddEvenFromArray(int[] arr, ArrayList list)
+        {
+            foreach (int n in arr)
+            {
+                if (n % 2 == 0) list.Add(n);
+            }
+        }
+
+        public static int[] ReverseArray(int[] arr)
+        {
+            int[] reversed = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                reversed[i] = arr[arr.Length - i - 1];
+            }
+
+            return reversed;
+        }
+
+        public static void ReverseInPlace(int[] arr)
+        {
+            int size = arr.Length;
+            for (int i = 0; i < size / 2; i++)
+            {
+                //swap arr[i] with index(arr[input.length - i - 1])
+                int temp = arr[i];
+                arr[i] = arr[size - i - 1];
+                arr[size - i - 1] = temp;
+            }
+            return;
+        }
+
+        /**
+        *   Sample of rotating an array in place 
+        *   as opposed to using a new array and return
+        */
+        public static void RotateLeft(int[] arr, int rotations)
+        {
+            int size = arr.Length;
+
+            //loop for number of total rotations
+            for (int i = 0; i < rotations; i++)
+            {
+                //take first item and save for last idex
+                int temp = arr[0];
+
+                //loop over array size - 1 // prevents overflow index
+                for (int k = 0; k < size - 1; k++)
+                {
+                    //push each item down 1 index
+                    arr[k] = arr[k + 1];
+                }
+
+                //append first item to end &  iter.
+                arr[size - 1] = temp;
+            }
+        }
+    }
+}
---
+++ Algorithms/Aggregation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = {-8,2,3,-9,11,-20};
+            int[] arr2 = {0,-2,-9,-39,39,10,7};
+
+            int[] arr3 = Aggregation.MergeEvenValues(arr1,arr2);
+            int[] arr4 = {1,2,3,4,5,6};
+            Aggregation.RotateLeft(arr4,1);
+            
+            foreach (int i in  arr4)
+                Console.WriteLine(i);
+        }
+    }
+}
