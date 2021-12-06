095406526e445f548e974e07198a41686dad0512
d0rf47 <m.jay.falconi@gmail.com>
2021-12-06 17:03:44 -0500

    queue basics added

--- Algorithms/Queue/Queue.cs
+++ Algorithms/Queue/Queue.cs
@@ -1,15 +1,33 @@
 using System;
-
+using DoublyLinkedList;
 
 /**
-*   Data Structure useful for order processing
-    Elems are added to back (enqueue) and removed from front (dequeue)
-    FIFO order
+* Data Structure useful for order processing
+* Elems are added to back (enqueue) and removed from front (dequeue)
+* incldes peek method to view item at front
+* FIFO order
 */
 namespace Queue
 {
-    class Queue
+    class Queue<T>
     {
-        
+        DList<T> list;
+
+        public void enqueue(T data)
+        {
+            list.PushBack(data);
+        }
+
+        public T dequeue()
+        {
+            Node<T> data = list.GetHead();
+            list.RemoveFirst();
+            return data.data;
+        }
+
+        public T peek()
+        {
+            return list.GetHead().data;
+        }
     }
 }
