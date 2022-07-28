51220b999b281121274b47ef27cd65a35259d73a
Michael Falconi <mfalconi@perle.com>
2022-07-28 15:50:32 -0400

    test

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
