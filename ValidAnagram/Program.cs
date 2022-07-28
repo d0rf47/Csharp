232b517f879915effb9cc21d7c87c6d8da3e0fbd
personal <islander91mike@gmail.com>
2022-07-28 16:13:03 -0400

    Added ValidAnagram Solution

---
+++ ValidAnagram/Program.cs
@@ -0,0 +1,31 @@
+﻿
+public class Solution
+{
+    public static bool IsAnagram(string s, string t) 
+    {
+        if(s.Length != t.Length)
+            return false;
+
+        int sCount = 0;
+        int tCount = 0;
+        HashSet<char> hashStrings = new HashSet<char>();
+        hashStrings = s.ToHashSet();
+        foreach(var c in hashStrings)
+        {
+            char x = c;
+            sCount = s.Count((l) => l == x);
+            tCount = t.Count((l) => l == x);
+            if(sCount != tCount)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void Main(String[] args)
+    {
+        string s = "rat";
+        string t = "car";
+        Console.WriteLine(IsAnagram(s,t));
+    }
+}
