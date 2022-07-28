6ba69cf2293301f0c0dc2e1204e441a7ec121046
personal <islander91mike@gmail.com>
2022-07-28 16:11:25 -0400

    added RunningSum Solution

---
+++ RunningSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+public class Solution {
+    public static int[] RunningSum(int[] nums) {
+        List<int> output = new ();
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(output.Count == 0)
+            {
+                output.Add(nums[i]);
+            }else
+            {
+                output.Add(output.Last() + nums[i]);
+            }
+        }
+        return output.ToArray();
+    }
+
+
+    static void Main(String[] args)
+    {
+        int[] nums = new int[] {1,2,3,4};
+        int[] output = RunningSum(nums);        
+        Console.WriteLine(output[output.Count() - 1]);
+    }
+
+}
