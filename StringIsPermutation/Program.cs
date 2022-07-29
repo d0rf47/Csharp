cd1e9d76b6c4e281fb2aa1a62f81002a692bf88d
personal <islander91mike@gmail.com>
2022-07-29 12:48:08 -0400

    Added StringIsPermutation

---
+++ StringIsPermutation/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    public static IList<string> FindAndReplacePattern(string[] words, string pattern)
+    {
+        List<string> returnValues = new ();
+        foreach(string w in words)
+        {
+            bool isValid = true;
+            for(int i = 1; i < pattern.Length; i++)
+            {                
+                if(pattern[i] == pattern[i-1] && w[i] != w[i-1])
+                    isValid = false;
+                if(pattern[i] != pattern[i-1] && w[i] == w[i-1])
+                    isValid = false;                                     
+                if( pattern.Where((c,index) => index != i).ToList().FindIndex((c) => c == pattern[i]) 
+                    != w.Where((c,index) => index != i).ToList().FindIndex((c) => c == w[i]) )
+                {
+                    isValid = false;   
+                }
+            }
+            if(isValid)
+                returnValues.Add(w);
+        }
+        return returnValues;
+    }
+
+    static void Main(string[] args)
+    {
+        string[] words = {"abc","cba","xyx","yxx","yyx"}; //["abc","deq","mee","aqq","dkd","ccc"]
+        string pattern = "abc"; //abb
+        List<string> output = FindAndReplacePattern(words, pattern).ToList();        
+        foreach(string s in output)
+            Console.WriteLine(s);
+    }
+}
