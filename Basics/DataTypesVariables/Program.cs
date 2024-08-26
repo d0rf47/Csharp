7bb49e89de7315fdc29f36d29e3e54ff48259917
personal <mfalconi@perle.com>
2024-08-26 16:58:21 -0400

    updated notes and sample code

---
+++ Basics/DataTypesVariables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace mfalconi.Basics.DataTypes
+{
+    /// <summary>
+    /// Basic overview of ValueTypes
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? nullableNumber = null;
+            int nonNullableNumber;            
+            nullableNumber = 99;
+            nonNullableNumber = nullableNumber ?? 10;
+            Console.WriteLine($"The value of the nonNullableNumber is {nonNullableNumber}");
+            if(nullableNumber.HasValue)
+                Console.WriteLine($"nullableNumber Value is {nullableNumber}");
+            else
+                Console.WriteLine($"nullableNumber variable has no value");
+            
+            nullableNumber = null;
+            nullableNumber ??=  nonNullableNumber = nullableNumber ?? 10;
+            Console.WriteLine($"The value of the nonNullableNumber is {nonNullableNumber}");
+            
+        }
+    }
+}
