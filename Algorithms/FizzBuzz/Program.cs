ec17ccf24b9becd65a85b99a81797a663d8a9cc7
d0rf47 <islander91mike@gmail.com>
2024-09-11 21:25:33 -0400

    fizzbuzz

--- Algorithms/FizzBuzz/Program.cs
+++ Algorithms/FizzBuzz/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Fizz
+{
+    class Program
+    {
+        public static void FizzBuzz(int n)
+        {
+            for(int i = 1; i < n + 1; i++){
+                if(i % 3 == 0 && i%5 == 0)
+                Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0 )
+                    Console.WriteLine("Fizz");
+                else if (i%5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program.FizzBuzz(10);
+        }
+    }
+}
