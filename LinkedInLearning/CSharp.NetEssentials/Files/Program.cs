9940eafa5cd4adf17fe314ac56609450271098bd
Michael Falconi <michael.falconi@constellationhb.com>
2024-11-27 14:08:21 -0500

    Added File Examples

---
+++ LinkedInLearning/CSharp.NetEssentials/Files/Program.cs
@@ -0,0 +1,34 @@
+﻿const string fileName = "TestFile.txt";
+string readFileData = string.Empty;
+string userInput = string.Empty;
+
+if(File.Exists(fileName))
+{    
+    File.WriteAllText(fileName, "The file has been overwritten");
+    File.AppendAllText(fileName, Environment.NewLine + "This has been appended");
+}
+else
+{
+    using(StreamWriter sw = File.CreateText(fileName))
+    {
+        sw.WriteLine("This is a text file!");        
+    }
+     
+}
+
+readFileData = File.ReadAllText(fileName);
+Console.WriteLine(readFileData);
+Console.WriteLine("Do you want to remove the file? Yes/No");
+
+userInput = Console.ReadLine();
+if(userInput.ToLower() == "yes" || userInput.ToLower() == "y")
+{
+    File.Delete(fileName);
+    Console.Clear();
+    Console.WriteLine("File Deleted!");
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("Goodbye World");
+}
