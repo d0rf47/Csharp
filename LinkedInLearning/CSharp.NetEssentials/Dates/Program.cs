1444efc17ad8056366c0ede540bf477c1573fff0
Michael Falconi <islander91mike@gmail.com>
2024-11-27 15:26:11 -0500

    added problem desc

--- LinkedInLearning/CSharp.NetEssentials/Dates/Program.cs
+++ LinkedInLearning/CSharp.NetEssentials/Dates/Program.cs
@@ -5,16 +5,21 @@
     /// If the date string provided is invalid
     /// return int.MaxValue
     /// </summary>
-    public class Answer {
+    public class Answer
+    {
         static DateTime today = DateTime.Now;
-    
-        public static int CalcHowManyDays(string date_str) {
+
+        public static int CalcHowManyDays(string date_str)
+        {
             // Your code goes here.
             var result = new DateTime();
             bool success = DateTime.TryParse(date_str, out result);
-            if(success){
-               return (today - result).Days;
-            }else{
+            if (success)
+            {
+                return (today - result).Days;
+            }
+            else
+            {
                 return int.MaxValue;
             }
         }
@@ -27,7 +32,7 @@
         {
             var learnerResult = new List<int>();
 
-            string[] test_dates = { 
+            string[] test_dates = {
                 "July 4, 1776",
                 "12/25/2025",
                 "April 1",
@@ -37,7 +42,8 @@
                 "Sat, 18 Aug 2018"
             };
 
-            foreach (string date in test_dates) {
+            foreach (string date in test_dates)
+            {
                 learnerResult.Add(Answer.CalcHowManyDays(date));
                 Console.WriteLine(learnerResult.Last());
             }
--- LinkedInLearning/CSharp.NetEssentials/Directories/Program.cs
+++ LinkedInLearning/CSharp.NetEssentials/Directories/Program.cs
@@ -2,8 +2,14 @@
 
 namespace Falconi.Csharp.NetEssentials
 {
+    /// <summary>
+    /// Write a solution to:
+    /// count the files in FileCollection Directory
+    /// count how many of each file type there is
+    /// Display the total size of each category in bytes
+    /// </summary>
     public class Answer
     {
-        
+
     }
 }
